10b38de8548dc9b7e2baa1a0daaf8a71dead54b9
Ernsyka Methelus <ernsykamethelus@yahoo.com>
2020-08-29 01:05:13 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,32 @@
+class Dog
+end
+
+def bark
+    puts "Woof!"
+  end
+  
+def play
+   puts "The Dog is playing"
+  end
+  
+fido = Dog.new
+puts fido
+
+snoopy = Dog.new
+puts snoopy
+
+lassie= Dog.new
+puts lassie
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ lib/person.rb
@@ -0,0 +1,44 @@
+class Person
+end
+
+ adele_goldberg = Person.new
+puts adele_goldberg
+
+alan_kay= Person.new
+puts alan_kay
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
